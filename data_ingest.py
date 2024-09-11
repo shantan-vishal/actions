3a2e0022b9823a31ee2eb0ee455293cb27b1eb11
shantan-vishal <vishalshantan2000@gmail.com>
2024-09-11 11:10:50 +0530

    Update data_ingest.py

--- data_ingest.py
+++ data_ingest.py
@@ -9,7 +9,7 @@ mongo_uri = "mongodb://zk_admin:Zkteco*123@10.6.1.179:27019/?authMechanism=DEFAU
 client = pymongo.MongoClient(mongo_uri)
 
 # Specify the database and collection
-db = client['your_database']  # Replace 'your_database' with the actual database name
+db = client['ralvie_db']  
 collection = db['your_collection']  # Replace 'your_collection' with the actual collection name
 
 # Fetch data from MongoDB
@@ -19,3 +19,4 @@ data = pd.DataFrame(list(collection.find()))
 data.to_csv('ingested_data.csv', index=False)
 
 print("Data ingestion completed and saved as ingested_data.csv")
+
